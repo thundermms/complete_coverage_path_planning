271905997312f536f8934a94950c34eb91e0ceaf
sundaram <sundaram.manickam@kabam.ai>
2023-11-28 09:58:19 +0800

    created client and encapsulated a ros trigger action

--- ipa_room_exploration/ros/src/room_exploration_action_client.cpp
+++ ipa_room_exploration/ros/src/room_exploration_action_client.cpp
@@ -15,156 +15,162 @@
 #include <geometry_msgs/Polygon.h>
 #include <geometry_msgs/Point32.h>
 #include <nav_msgs/OccupancyGrid.h>
-
+#include <sensor_msgs/Image.h>
 #include <ipa_building_msgs/RoomExplorationAction.h>
 #include <ipa_room_exploration/dynamic_reconfigure_client.h>
 #include <ipa_room_exploration/timer.h>
 #include <Eigen/Dense>
+#include <boost/shared_ptr.hpp>
 
-// overload of << operator for geometry_msgs::Pose2D to wanted format
-std::ostream& operator<<(std::ostream& os, const geometry_msgs::Pose2D& obj)
-{
-	std::stringstream ss;
-	ss <<  "[" << obj.x << ", " << obj.y << ", " << obj.theta << "]";
-	os << ss.rdbuf();
-	return os;
-}
+#include <nav_msgs/Odometry.h>
+#include <tf/transform_datatypes.h>
 
-int main(int argc, char **argv)
-{
-	ros::init(argc, argv, "room_exploration_client");
-	ros::NodeHandle priv_nh("~");
-	ros::NodeHandle nh;
+#include "std_srvs/Trigger.h"
 
-	actionlib::SimpleActionClient<ipa_building_msgs::RoomExplorationAction> ac("room_exploration_server", true);
 
-	// read params
-	bool use_test_maps;
-	priv_nh.param("use_test_maps", use_test_maps, true);
-	double resolution;
-	priv_nh.param("resolution", resolution, 0.05);
-	std::vector<double> origin (3,0);
-	priv_nh.param("origin", origin, origin);
-	double robot_radius;
-	priv_nh.param("robot_radius", robot_radius, 0.3);
-	double coverage_radius;
-	priv_nh.param("coverage_radius", coverage_radius, 1.0);
-	std::vector<double> start_pos = {1, 1, 0};
-	priv_nh.param("starting_position", start_pos, start_pos);
+class RoomExplorationClient {
+public:
+   RoomExplorationClient() : nh(), priv_nh("~"),ac("room_exploration_server", true) {
+        ROS_INFO("Waiting for action server to start.");
+        ac.waitForServer(); // will wait for infinite time
+        ROS_INFO("Action server started");
 
-	if (start_pos.size() != 3)
-	{
-		ROS_FATAL("starting_position must contain 3 values");
-		return -1;
-	}
+        // Advertise the auto_path_planning service
+        service = nh.advertiseService("/auto_path_planning", &RoomExplorationClient::autoPathPlanningCallback, this);
+    }
 
-	std::string image_path;
-	if (use_test_maps)
-	{
-		// read in test map
-		const std::string test_map_path = ros::package::getPath("ipa_room_segmentation") + "/common/files/test_maps/";
-		image_path = test_map_path + "lab_ipa.png";
-	}
-	else
-	{
-		std::string env_pack_path;
-		priv_nh.param<std::string>("env_pack", env_pack_path, "ipa_room_segmentation");
-		std::string file_name;
-		priv_nh.param<std::string>("image", file_name, "map.pgm");
-		std::string map_name;
-		priv_nh.param<std::string>("robot_env", map_name, "lab_ipa");
+private:
+    ros::NodeHandle nh;
+    ros::NodeHandle priv_nh;
+    ros::ServiceServer service;
+    ros::Subscriber sub_map;
+    ros::Subscriber sub_odom;
 
-		image_path = env_pack_path + "/envs/" + map_name + "/" + file_name;
-	}
+    actionlib::SimpleActionClient<ipa_building_msgs::RoomExplorationAction> ac;
+    sensor_msgs::Image global_labeling;
+    nav_msgs::OccupancyGrid::ConstPtr global_occupancy_grid;
+    geometry_msgs::Pose2D current_robot_pose;
+    bool isDataReady;
 
-	cv::Mat map_flipped = cv::imread(image_path, 0);
-	cv::Mat map;
-	cv::flip(map_flipped, map, 0);
-	//make non-white pixels black
-	for (int y = 0; y < map.rows; y++)
-	{
-		for (int x = 0; x < map.cols; x++)
-		{
-			//find not reachable regions and make them black
-			if (map.at<unsigned char>(y, x) < 250)
-			{
-				map.at<unsigned char>(y, x) = 0;
-			}
-			//else make it white
-			else
-			{
-				map.at<unsigned char>(y, x) = 255;
-			}
-		}
-	}
-	std::cout << "map-size: " << map.rows << "x" << map.cols << std::endl;
-	ROS_INFO("Waiting for action server to start.");
-	// wait for the action server to start
-	ac.waitForServer(); //will wait for infinite time
+    // Callback function for the map subscription
+    void mapCallback(const nav_msgs::OccupancyGrid::ConstPtr& msg)
+    {   ROS_INFO("getting image");
+        global_occupancy_grid = msg;
+        cv::Mat map(global_occupancy_grid->info.height, global_occupancy_grid->info.width, CV_8UC1);
 
-	ROS_INFO("Action server started, sending goal.");
+        for (int y = 0; y < map.rows; y++)
+        {
+            for (int x = 0; x < map.cols; x++)
+            {
+                int8_t occupancy_value = global_occupancy_grid->data[y * global_occupancy_grid->info.width + x];
 
-	DynamicReconfigureClient drc_exp(nh, "room_exploration_server/set_parameters", "room_exploration_server/parameter_updates");
-	drc_exp.setConfig("path_eps", 200.0);
-	drc_exp.setConfig("map_correction_closing_neighborhood_size", 4);
+                // Convert the occupancy grid values to the required image format
+                if (occupancy_value == -1)
+                    map.at<uchar>(y, x) = 127;  // Unknown space, use gray
+                else if (occupancy_value <= 50)
+                    map.at<uchar>(y, x) = 255;  // Free space
+                else
+                    map.at<uchar>(y, x) = 0;    // Obstacle
+            }
+        }
+        ROS_INFO("getting image");
+        // Convert the map to sensor_msgs::Image
+        cv_bridge::CvImage cv_image;
+        cv_image.encoding = "mono8";
+        cv_image.image = map;
 
-	sensor_msgs::Image labeling;
-	cv_bridge::CvImage cv_image;
-//	cv_image.header.stamp = ros::Time::now();
-	cv_image.encoding = "mono8";
-	cv_image.image = map;
-	cv_image.toImageMsg(labeling);
+        sensor_msgs::Image labeling;
+        cv_image.toImageMsg(labeling);
+        global_labeling = labeling;
+        isDataReady = true;
+    }
+    void odomCallback(const nav_msgs::Odometry::ConstPtr& odomMsg)
+    {   ROS_INFO("getting odom");
+        // Assuming the odom message contains pose information
+        current_robot_pose.x = odomMsg->pose.pose.position.x;
+        current_robot_pose.y = odomMsg->pose.pose.position.y;
+        // Assuming the orientation in the quaternion is represented as (x, y, z, w)
+        current_robot_pose.theta = tf::getYaw(odomMsg->pose.pose.orientation);
+    }
 
-	geometry_msgs::Pose map_origin;
-	map_origin.position.x = origin[0];
-	map_origin.position.y = origin[1];
-	map_origin.position.z = origin[2];
+    bool autoPathPlanningCallback(std_srvs::Trigger::Request& req,
+                                std_srvs::Trigger::Response& res)
+    {  
+        // actionlib::SimpleActionClient<ipa_building_msgs::RoomExplorationAction> ac("room_exploration_server", true);
+        // Subscribe to the slamware_ros_sdk_server_node.map topic
+        sub_map = nh.subscribe("/slamware_ros_sdk_server_node/map", 1, &RoomExplorationClient::mapCallback,this);
+        sub_odom = nh.subscribe("/slamware_ros_sdk_server_node/odom", 1,  &RoomExplorationClient::odomCallback, this);
 
-	geometry_msgs::Pose2D starting_position;
-	starting_position.x = start_pos[0];
-	starting_position.y = start_pos[1];
-	starting_position.theta = start_pos[2];
+        // read params
+        bool use_test_maps;
+        priv_nh.param("use_test_maps", use_test_maps, true);
+        double resolution;
+        priv_nh.param("resolution", resolution, 0.05);
+        std::vector<double> origin (3,0);
+        priv_nh.param("origin", origin, origin);
+        double robot_radius;
+        priv_nh.param("robot_radius", robot_radius, 0.3);
+        double coverage_radius;
+        priv_nh.param("coverage_radius", coverage_radius, 1.0);
+        
+        // Create a CvImage to hold the received map
+        cv_bridge::CvImagePtr cv_image_ptr;
+        ROS_INFO("i AM HERE");
+        // Example: Accessing the map data (assuming global_labeling is a global variable)
+        if (!global_labeling.data.empty())
+        {
+        cv_image_ptr = cv_bridge::toCvCopy(global_labeling, "mono8");
+        cv::Mat map_image = cv_image_ptr->image;
 
-	std::vector<geometry_msgs::Point32> fov_points(4);
+            // Now you can use 'map_image' as the input_map for the room exploration goal
+        }
+        
 
-	int planning_mode = 1;
-	geometry_msgs::Point32 fov_origin;
-	fov_origin.x = 0.;
-	fov_origin.y = 0.;
+        while(!isDataReady)
+        {   ROS_INFO("stuck");
+            ros::spinOnce();
+        }
+        isDataReady = false;
+        DynamicReconfigureClient drc_exp(nh, "room_exploration_server/set_parameters", "room_exploration_server/parameter_updates");
+        drc_exp.setConfig("path_eps", 200.0);
+        drc_exp.setConfig("map_correction_closing_neighborhood_size", 4);
+        std::vector<geometry_msgs::Point32> fov_points(4);
 
-	ipa_building_msgs::RoomExplorationGoal goal;
-	goal.input_map = labeling;
-	goal.map_resolution = resolution;
-	goal.map_origin = map_origin;
-	goal.robot_radius = robot_radius; // turtlebot, used for sim 0.177, 0.4
-	goal.coverage_radius = coverage_radius;
-	goal.field_of_view = fov_points;
-	goal.field_of_view_origin = fov_origin;
-	goal.starting_position = starting_position;
-	goal.planning_mode = planning_mode;
-	ac.sendGoal(goal);
+        int planning_mode = 1;
+        geometry_msgs::Point32 fov_origin;
+        fov_origin.x = 0.;
+        fov_origin.y = 0.;
+        ipa_building_msgs::RoomExplorationGoal goal;
+        goal.input_map = global_labeling;
+        goal.map_resolution =global_occupancy_grid->info.resolution;
+        goal.map_origin = global_occupancy_grid->info.origin;
+        goal.robot_radius = robot_radius; // turtlebot, used for sim 0.177, 0.4
+        goal.coverage_radius = coverage_radius;
+        goal.field_of_view = fov_points;
+        goal.field_of_view_origin = fov_origin;
+        goal.starting_position = current_robot_pose;
+        goal.planning_mode = planning_mode;
+        // shutdown the subscriber (this should be unnecessary)
+        sub_map.shutdown();
+        sub_odom.shutdown();
+        ac.sendGoal(goal);
 
-	ac.waitForResult(ros::Duration());
-	ipa_building_msgs::RoomExplorationResultConstPtr action_result = ac.getResult();
+        
 
-	std::cout << "Got a path with " << action_result->coverage_path.size() << " nodes." << std::endl;
+        // Populate the response if needed
+        res.success = true;
+        res.message = "Auto path planning completed successfully";
 
-	// display path
-	const double inverse_map_resolution = 1./goal.map_resolution;
-	cv::Mat path_map = map.clone();
-	for (size_t point=0; point<action_result->coverage_path.size(); ++point)
-	{
-		const cv::Point point1((action_result->coverage_path[point].x-map_origin.position.x)*inverse_map_resolution, (action_result->coverage_path[point].y-map_origin.position.y)*inverse_map_resolution);
-		cv::circle(path_map, point1, 2, cv::Scalar(128), -1);
-		if (point > 0)
-		{
-			const cv::Point point2((action_result->coverage_path[point-1].x-map_origin.position.x)*inverse_map_resolution, (action_result->coverage_path[point-1].y-map_origin.position.y)*inverse_map_resolution);
-			cv::line(path_map, point1, point2, cv::Scalar(128), 1);
-		}
-		std::cout << "coverage_path[" << point << "]: x=" << action_result->coverage_path[point].x << ", y=" << action_result->coverage_path[point].y << ", theta=" << action_result->coverage_path[point].theta << std::endl;
-	}
-	cv::imshow("path", path_map);
-	cv::waitKey();
+        return true;
 
-	return 0;
+    }
+
+};
+
+
+int main(int argc, char** argv) {
+    ros::init(argc, argv, "room_exploration_client");
+    RoomExplorationClient client;
+    ros::spin(); // Keep the node alive
+    return 0;
 }
